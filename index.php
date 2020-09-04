d986f9e429ad69d28ca2967ce7a55bb6e2885135
Carlos Carrete <carlos123edd@gmail.com>
2020-09-04 13:00:02 -0500

    Segundo commit

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <title>Productos de CVA</title>
+    <link rel="stylesheet" href="css/estilos.css" />
+    <meta
+      name="viewport"
+      content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"
+    />
+  </head>
+  <body>
+    <h1 class="title">Productos de CVA</h1>
+
+    <div class="container">
+      <div class="card">
+        <img src="http://www.grupocva.com/detalle_articulo/5925687.jpg" />
+        <h4>AIRE ACONDICIONADO STULZ</h4>
+        <h6>
+          AIRE ACONDICIONADO DE PRESICIÓN STULZ MODELO CYBERAIR3 CAPACIDAD 20 TR
+          (3 BULTOS)
+        </h6>
+        <a href="#">39368.1900 Dolares</a>
+      </div>
+
+      <div class="card">
+        <img src="http://www.grupocva.com/detalle_articulo/5925687.jpg" />
+        <h4>AIRE ACONDICIONADO STULZ</h4>
+        <h6>
+          AIRE ACONDICIONADO DE PRESICIÓN STULZ MODELO CYBERAIR3 CAPACIDAD 20 TR
+          (3 BULTOS)
+        </h6>
+        <a href="#">39368.1900 Dolares</a>
+      </div>
+
+      <div class="card">
+        <img src="http://www.grupocva.com/detalle_articulo/5925687.jpg" />
+        <h4>AIRE ACONDICIONADO STULZ</h4>
+        <h6>
+          AIRE ACONDICIONADO DE PRESICIÓN STULZ MODELO CYBERAIR3 CAPACIDAD 20 TR
+          (3 BULTOS)
+        </h6>
+        <a href="#">39368.1900 Dolares</a>
+      </div>
+    </div>
+  </body>
+</html>
